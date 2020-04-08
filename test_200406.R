752c93514d4ad96d62319466b66c9a8fcab174e6
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-04-08 21:04:53 +0900

    0408

--- test_200406.R
+++ test_200406.R
@@ -33,3 +33,65 @@ a <- a+1
 typeof(a)
 a <- as.integer(a)
 typeof(a)
+a <- as.character(a)
+is.character(a)
+is.integer(a)
+c <- is.integer(a)
+typeof(c)
+rm(c)
+
+a <- as.double(a)
+typeof(a)
+is.double(a)
+
+a <- as.complex(a)
+typeof(a)
+a
+
+a<- as.character(a)
+
+a<- T
+typeof(a)
+is.logical(a)
+a <- as.integer(a)
+a <- as.logical(a)
+
+str(b)
+
+
+# 객체
+# 벡터
+x <- c(1,2,3)
+x
+x <- c(1)
+x <- 1
+x
+class(x)
+x <- c(1,2,3)
+y <- c(x,4,5,6)
+y
+z <- c(x,y)
+z
+#z <- (1,2) error
+str(z)
+
+#데이터 선택
+z
+z[1]
+z[2]
+z[3:6]
+
+# seq, rep
+# seq
+seq(10)
+a <- seq(5,10)
+a
+seq(1,10,2)
+seq(20,0,-2)
+seq(from=0,to=20,by=2)
+
+#rep
+rep(1,100)
+rep(1,times=100)
+a<-rep(1,100)
+a
